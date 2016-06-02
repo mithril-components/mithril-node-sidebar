d336c7a5840de90cf7ab36fe89b0901439003243
funa <funa@web-presence-in-china.com>
2016-06-02 11:08:48 +0800

    fix submenu active style

--- test.js
+++ test.js
@@ -37,7 +37,7 @@ const menus = [
 const model = {
     title: 'Web site title',
     logo: 'glyphicon glyphicon-list-alt',
-    active: '/some/url/',
+    active: '/some/url/of/a/page',
     inner: {
         controller: () => {
             return {
