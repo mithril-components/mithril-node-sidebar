5a22c7e2acd224bfad9833f81e56103196bbbf36
Funa <geniusfuna@163.com>
2016-08-03 17:02:47 +0800

    fix directorylist & layout

--- mithril_components/directorylist/directorylist.js
+++ mithril_components/directorylist/directorylist.js
@@ -11,14 +11,21 @@ const controller = (list) => {
 const view = (ctrl) => {
     // generate the page
     // console.log(ctrl.length);
-    return m('div.col-md-12.mc-sidebar-dir', 
-        ctrl.map(directoryCtrl => {
-            var n = Math.floor(12/ Math.ceil(Math.sqrt(ctrl.length)));
-            // console.log(n);
-            return m('div.col-md-'+ n + '.menuicon.text-center',
-                directory.view(directoryCtrl)
-            )
-        })
+    return m('div.col-md-12.mc-sidebar-dir',
+        m('div.row',
+            m('div.col-lg-2.hidden-xs',''),
+            m('div.col-lg-8.col-xs-12',
+                ctrl.map(directoryCtrl => {
+                    var n = Math.floor(12/ Math.ceil(Math.sqrt(ctrl.length)));
+                    // console.log(n);
+                    return m('div.col-md-'+ n + '.col-xs-6.menuicon.text-center',
+                        directory.view(directoryCtrl)
+                    )
+                })
+            ),
+            m('div.col-lg-2.hidden-xs','')
+        )
+        
     )
     
 }
